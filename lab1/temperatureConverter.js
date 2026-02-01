ccdd1e731fcf846e6f8cae68c0b2b62a74cd4298
ektathapa <np01mm4a210038@islingtoncollege.edu.np>
2026-02-01 12:56:14 -0800

    updated

---
+++ lab1/temperatureConverter.js
@@ -0,0 +1,8 @@
+let fahrenheit = 50; // Average temperature in Kathmandu in January
+
+let celsius = (fahrenheit - 32) * 5 / 9;
+let kelvin = celsius + 273.15;
+
+console.log("Temperature (Fahrenheit): " + fahrenheit);
+console.log("Temperature (Celsius): " + celsius);
+console.log("Temperature (Kelvin): " + kelvin);
